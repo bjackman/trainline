996753824051504d8ae5925ae271349794552dc2
Brendan Jackman <bhenryj0117@gmail.com>
2015-03-20 14:46:29 +0000

    init

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject trainline "0.1.0-SNAPSHOT"
+  :description "Convert email webhooks to event webhooks"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :main ^:skip-aot trainline.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
